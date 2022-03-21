ae44cedffdc153d5c057a5123828349ef7c8d170
janickq <81129332+janickq@users.noreply.github.com>
2022-03-21 19:41:05 +0800

    added some source pics

---
+++ Resize.py
@@ -0,0 +1,8 @@
+import cv2
+import glob
+
+for filename in glob.glob('WOB\WOB2\*.jpg'): # path to your images folder
+    print(filename)
+    img=cv2.imread(filename) 
+    rl=cv2.resize(img, (1024,1024))
+    cv2.imwrite(f'{filename}resized.jpg', rl)
